559ecd8906a68e497047cca02b167c7ac7e2405e
eleventhbox <34207525+eleventhbox@users.noreply.github.com>
2019-12-04 22:45:24 +0300

    lesson_1

---
+++ task_1.py
@@ -0,0 +1,8 @@
+a = 1
+b = "test"
+print('Value of a is ' + str(a))
+print(f'Value of b is {b}')
+num1 = int(input('Enter num1 of type int: '))
+num2 = float(input('Enter num2 of type float: '))
+print("Value of num1 is {}".format(num1))
+print("Value of num1 is {:.3f}".format(num2))
